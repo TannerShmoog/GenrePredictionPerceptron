12ab9b422549072768c90b0daee80e085a16ec21
TannerShmoog <tannershmoog@gmail.com>
2018-06-06 02:33:23 -0600

    prediction function added

--- Perceptron.py
+++ Perceptron.py
@@ -28,8 +28,6 @@ def new_weights():
             weightFile.write(str(i))
             weightFile.write(',')
 
-
-
 #returns an array of amplitudes normalized to 0.0 to 1.0
 #max frequency i care about is ~10k, 
 def get_data_point(filename, root):  
@@ -42,24 +40,38 @@ def get_data_point(filename, root):
     end_frame = start_frame + (MAX_VALS * 2)
     
     arr =  data[start_frame:end_frame]
-    
     #normalize values
     for i in range(len(arr)):
         arr[i] = (arr[i] - MIN) / (MAX - MIN)
     return arr[1::2]
 
+#predict a wav file
+def predict(data_point, weights):
+    activation = weights[0]
+    for i in range(len(data_point)):
+        activation += weights[i+1] * data_point[i]
+    if activation >= 0.0:
+        return 0
+    else:
+        return 1
+    
 weights = read_weights()
+l_rate = 0.2
 
-for i in range(100):
+
+for i in range(10):
     seed = random.randint(0,1)
     if seed == 0:
         root = rootTrue
     elif seed == 1:
         root = rootFalse
     filename = random.choice(os.listdir(root))
-    #print(len(get_data_point(filename, root)))
-    print(len(weights))
-    
+    data_array = get_data_point(filename, root)
+    prediction = predict(data_array, weights)
+    if seed =/= prediction:
+        #train the weights for wrong answer
+        pass
+    print("Expected: " + str(seed) +"\tPredicted: " + str(prediction))
 
 done = time.time()
 print(done-start)
